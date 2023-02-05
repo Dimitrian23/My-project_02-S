581927f96bfead7730ae46bb4ba056a69d97d3de
Dmitrian23 <dimonio1991@mail.ru>
2023-02-05 21:51:13 +0300

    Задача 022

---
+++ 022/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача_№22 По двум числам проверить является ли одно квадратом другого.
+
+int a,b,s;
+
+        System.Console.WriteLine("Введите в терминал последовательно два числа, подтверждая ввод каждого нажатием клавиши 'Enter' ");
+             a=Convert.ToInt32(Console.ReadLine());
+             b=Convert.ToInt32(Console.ReadLine());
+ 
+s=a/b;
+  if(b==s) 
+    {
+       System.Console.WriteLine($"Число ({a}) является квадратом числа ({b})"); 
+    }
+  else
+    { 
+       System.Console.WriteLine($"Число ({a}) не является квадратом числа ({b})");
+    }
+       System.Console.WriteLine("Не забудь протестировать меня введя и другие числа!");
+       
