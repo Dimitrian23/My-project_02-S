f4cf2d33ff15b78140c6a7cb628148c045eba5f9
Dmitrian23 <dimonio1991@mail.ru>
2023-02-05 21:52:16 +0300

    Задача 025

---
+++ 025/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача_№25 Задать номер четверти, показать диапазоны для возможных координат
+  
+int quarter;
+        System.Console.WriteLine("Введите через терминал номер четверти, а я покажу диапозон возможных координат (x)(y)");
+         quarter=Convert.ToInt32(Console.ReadLine());
+
+    if (quarter==1) 
+       {
+        System.Console.WriteLine($"Диапазон возможных координат для ({quarter})_й четверти x>0 and y>0");
+       }
+    if (quarter==2)
+       {
+        System.Console.WriteLine($"Диапазон возможных координат для ({quarter})_й четверти  x<0 and y>0");
+       }
+    if (quarter==3) 
+       {
+        System.Console.WriteLine($"Диапазон возможных координат для ({quarter})_й четверти x<0 and y<0");
+       }
+    if (quarter==4) 
+       {
+        System.Console.WriteLine($"Диапазон возможных координат для ({quarter}) четверти x>0 and y<0");
+       }
+    if (quarter<=0 || quarter>=5)
+       {
+        System.Console.WriteLine("Неверно задали номер четверти! Значения должны быть в диапазоне (от 1 до 4)");
+       }
